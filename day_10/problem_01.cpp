ce1d616b7a34419dd108d6d82e52381e51d9a0d8
Tirtha <tirthasg@gmail.com>
2022-09-29 04:19:50 +0530

    day_10/problem_01.cpp code cleaned up and readability improved

--- day_10/problem_01.cpp
+++ day_10/problem_01.cpp
@@ -1,52 +1,52 @@
 #include <iostream>
 using namespace std;
 
-int AnyBaseAddition(int base, int num1, int num2)
+int AnyBaseAddition(int base, int number1, int number2)
 {
     int result = 0, carry = 0, ten_power = 1;
-    while (!(num1 == 0 && num2 == 0 && carry == 0)) {
-        int ld1 = num1 % 10;
-        int ld2 = num2 % 10;
+    while (!(number1 == 0 && number2 == 0 && carry == 0)) {
+        int ld1 = number1 % 10;
+        int ld2 = number2 % 10;
         
         int sum = ld1 + ld2 + carry;
         result += (sum % base) * ten_power;
         carry = sum / base;
         
-        num1 /= 10;
-        num2 /= 10;
+        number1 /= 10;
+        number2 /= 10;
         ten_power *= 10;
     }
     
     return result;
 }
 
-int AnyBaseMultiplicationByDigit(int base, int num1, int ld2)
+int AnyBaseMultiplicationByDigit(int base, int number1, int ld2)
 {
     int result = 0, carry = 0, ten_power = 1;
-    while (!(num1 == 0 && carry == 0)) {
-        int ld1 = num1 % 10;
+    while (!(number1 == 0 && carry == 0)) {
+        int ld1 = number1 % 10;
         int prod = ld1 * ld2 + carry;
         
         result += (prod % base) * ten_power;
         carry = prod / base;
         
-        num1 /= 10;
+        number1 /= 10;
         ten_power *= 10;
     }
     
     return result;
 }
 
-int AnyBaseMultiplication(int base, int num1, int num2)
+int AnyBaseMultiplication(int base, int number1, int number2)
 {
     int result = 0, ten_power = 1;
-    while (num2 > 0) {
-        int ld2 = num2 % 10;
-        int partial_prod = AnyBaseMultiplicationByDigit(base, num1, ld2) * ten_power;
+    while (number2 > 0) {
+        int ld2 = number2 % 10;
+        int partial_prod = AnyBaseMultiplicationByDigit(base, number1, ld2) * ten_power;
 
         result = AnyBaseAddition(base, result, partial_prod);
         
-        num2 /= 10;
+        number2 /= 10;
         ten_power *= 10;
     }
     
@@ -55,10 +55,10 @@ int AnyBaseMultiplication(int base, int num1, int num2)
 
 int main()
 {
-    int base, num1, num2;
-    cin >> base >> num1 >> num2;
+    int base, number1, number2;
+    cin >> base >> number1 >> number2;
     
-    int result = AnyBaseMultiplication(base, num1, num2);
+    int result = AnyBaseMultiplication(base, number1, number2);
     cout << result << endl;
     
     return 0;
